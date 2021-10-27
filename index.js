592447396016d62bf317e2c38529613d745af650
gustavoisaacc <gustavochaile7@gmail.com>
2021-10-27 15:51:57 -0300

    ajustar cors

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const app = express();
 conectDB();
 
 //habilitar cors
-app.use(cors());
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 //habilitar express.json
 app.use(express.json({extended: true}));
