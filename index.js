6bd6016f588474f7c9decf62f8d84d67741d3d74
gustavoisaacc <gustavochaile7@gmail.com>
2021-10-29 07:35:17 -0300

    corriciendo puerto

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors())
 app.use(express.json({extended: true}));
 
 //puerto de la app
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 //importa rutas
 app.use('/api/usuario', require('./routes/usuario'));
